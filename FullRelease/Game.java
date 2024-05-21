90add0b242644adaf7a80f91f43640e511a96eb6
LukasKubela <165004480+LukasKubela@users.noreply.github.com>
2024-05-21 15:22:43 +0200

    Update Game.java

--- FullRelease/Game.java
+++ FullRelease/Game.java
@@ -3,16 +3,20 @@ package FullRelease;
 import javax.swing.*;
 
 public class Game {
+
+    public static int playerWins = 0;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("6 Shot Roulette");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setResizable(false);
             frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+            MenuPanel menuPanel = new MenuPanel(frame);
+            frame.setContentPane(menuPanel);
+
             frame.setVisible(true);
-            frame.add(new FullRelease.MenuPanel(frame));
-            frame.validate();
         });
     }
 }
-// FIXME: Magnifying Glass doesn't work properly, and fix the Freeze
