976f3c09cf7a519c680af3b6bba358894cdde05c
LukasKubela <165004480+LukasKubela@users.noreply.github.com>
2024-05-21 15:24:25 +0200

    Update MenuPanel.java

--- FullRelease/MenuPanel.java
+++ FullRelease/MenuPanel.java
@@ -13,27 +13,20 @@ public class MenuPanel extends JPanel {
 
     private void initializeUI() {
         setLayout(new BorderLayout());
-        JLabel menuLabel = new JLabel("MENU", SwingConstants.CENTER);
-        menuLabel.setFont(new Font("Arial", Font.BOLD, 50));
-        add(menuLabel, BorderLayout.NORTH);
 
-        JButton playButton = new JButton("Play");
-        playButton.addActionListener(e -> switchToElevatorPanel());
-        add(playButton, BorderLayout.CENTER);
+        GameGraphics gameGraphics = new GameGraphics();
+        add(gameGraphics, BorderLayout.CENTER);
 
-        JButton settingsButton = new JButton("Settings");
-        settingsButton.addActionListener(e -> switchToSettingsPanel());
-        add(settingsButton, BorderLayout.SOUTH);
+        JButton playButton = gameGraphics.getPlayButton();
+        if (playButton != null) {
+            playButton.addActionListener(e -> switchToElevatorPanel());
+        }
     }
 
     private void switchToElevatorPanel() {
-        ElevatorPanel elevatorPanel = new FullRelease.ElevatorPanel(frame, this);
+        GamePanel gamePanel = new GamePanel(frame, this);
+        ElevatorPanel elevatorPanel = new ElevatorPanel(this, gamePanel, frame);
         updateContentPane(elevatorPanel);
-    }
-
-    private void switchToSettingsPanel() {
-        SettingsPanel settingsPanel = new SettingsPanel(frame, this);
-        updateContentPane(settingsPanel);
     }
 
     public void updateContentPane(JPanel newPanel) {
