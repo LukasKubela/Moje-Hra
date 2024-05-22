287cefe305afe9bef2724d91bd3029f58690cfed
LukasKubela <165004480+LukasKubela@users.noreply.github.com>
2024-05-22 16:12:38 +0200

    Update GameGraphics.java

--- FullRelease/GameGraphics.java
+++ FullRelease/GameGraphics.java
@@ -19,19 +19,41 @@ public class GameGraphics extends JPanel {
     private ImageIcon walkingImageLeft;
     private ImageIcon walkingImageRight;
     private BufferedImage elevatorImage;
+    private JButton playerButton;
+    private JButton enemyButton;
+    private JButton shopButton;
+    private BufferedImage casinoChip;
+    private BufferedImage secondCasinoChip;
+    private BufferedImage thirdCasinoChip;
+    private BufferedImage backButtonImage;
+    private JButton backButton;
+    private BufferedImage magnifyingGlassImage;
+    private JButton magnifyingGlassButton;
+    private BufferedImage freezeImage;
+    private JButton freezeButton;
+    private BufferedImage shootSignImage;
 
     public GameGraphics() {
         setLayout(new GridBagLayout());
         loadImage();
-        initializeButton();
+        initializeButtons();
         loadPlayerImages();
         loadElevatorImage();
+        loadCasinoChip();
+        loadSecondCasinoChip();
+        loadThirdCasinoChip();
+        loadBackButtonImage();
+        loadMagnifyingGlassImage();
+        loadFreezeImage();
+        initializeBackButton();
+        initializeMagnifyingGlassButton();
+        initializeFreezeButton();
+        loadShootSignImage();
     }
 
     private void loadImage() {
         try {
             backgroundImage = ImageIO.read(getClass().getResource("/table better.png"));
-            System.out.println("Background image loaded.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,13 +65,68 @@ public class GameGraphics extends JPanel {
             standingImage2 = ImageIO.read(getClass().getResource("/player standing 2.png"));
             walkingImageRight = new ImageIcon(getClass().getResource("/player walking 2.gif"));
             walkingImageLeft = new ImageIcon(getClass().getResource("/player walking.gif"));
-            System.out.println("Player images loaded.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void initializeButton() {
+    private void loadCasinoChip() {
+        try {
+            casinoChip = ImageIO.read(getClass().getResource("/casino chip.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadSecondCasinoChip() {
+        try {
+            secondCasinoChip = ImageIO.read(getClass().getResource("/casino chip.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadThirdCasinoChip() {
+        try {
+            thirdCasinoChip = ImageIO.read(getClass().getResource("/casino chip.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadShootSignImage() {
+        try {
+            shootSignImage = ImageIO.read(getClass().getResource("/SHOOT sign.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadBackButtonImage() {
+        try {
+            backButtonImage = ImageIO.read(getClass().getResource("/Back.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadMagnifyingGlassImage() {
+        try {
+            magnifyingGlassImage = ImageIO.read(getClass().getResource("/Magnifying glass.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFreezeImage() {
+        try {
+            freezeImage = ImageIO.read(getClass().getResource("/Freeze.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeButtons() {
         try {
             BufferedImage playButtonImage = ImageIO.read(getClass().getResource("/PLAY button.png"));
             originalIcon = new ImageIcon(playButtonImage);
@@ -80,10 +157,53 @@ public class GameGraphics extends JPanel {
             gbc.anchor = GridBagConstraints.CENTER;
 
             add(playButton, gbc);
-            System.out.println("Play button initialized.");
+
+            BufferedImage playerButtonImage = ImageIO.read(getClass().getResource("/YOU button.png"));
+            BufferedImage enemyButtonImage = ImageIO.read(getClass().getResource("/ENEMY button.png"));
+            BufferedImage shopButtonImage = ImageIO.read(getClass().getResource("/SHOP Button.png"));
+            playerButton = createHoverButton(playerButtonImage);
+            enemyButton = createHoverButton(enemyButtonImage);
+            shopButton = createHoverButton(shopButtonImage);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void initializeBackButton() {
+        backButton = createHoverButton(backButtonImage);
+    }
+
+    private void initializeMagnifyingGlassButton() {
+        magnifyingGlassButton = createHoverButton(magnifyingGlassImage);
+    }
+
+    private void initializeFreezeButton() {
+        freezeButton = createHoverButton(freezeImage);
+    }
+
+    private JButton createHoverButton(BufferedImage buttonImage) {
+        ImageIcon originalIcon = new ImageIcon(buttonImage);
+        ImageIcon darkerIcon = new ImageIcon(createDarkerImage(buttonImage));
+
+        JButton button = new JButton(originalIcon);
+        button.setPreferredSize(new Dimension(originalIcon.getIconWidth(), originalIcon.getIconHeight()));
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setIcon(darkerIcon);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setIcon(originalIcon);
+            }
+        });
+
+        return button;
     }
 
     private BufferedImage createDarkerImage(BufferedImage original) {
@@ -94,7 +214,6 @@ public class GameGraphics extends JPanel {
     private void loadElevatorImage() {
         try {
             elevatorImage = ImageIO.read(getClass().getResource("/Elevator.png"));
-            System.out.println("Elevator image loaded.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -130,5 +249,45 @@ public class GameGraphics extends JPanel {
 
     public BufferedImage getElevatorImage() {
         return elevatorImage;
+    }
+
+    public JButton getPlayerButton() {
+        return playerButton;
+    }
+
+    public JButton getEnemyButton() {
+        return enemyButton;
+    }
+
+    public JButton getShopButton() {
+        return shopButton;
+    }
+
+    public BufferedImage getCasinoChip() {
+        return casinoChip;
+    }
+
+    public BufferedImage getSecondCasinoChip() {
+        return secondCasinoChip;
+    }
+
+    public BufferedImage getThirdCasinoChip() {
+        return thirdCasinoChip;
+    }
+
+    public JButton getBackButton() {
+        return backButton;
+    }
+
+    public JButton getMagnifyingGlassButton() {
+        return magnifyingGlassButton;
+    }
+
+    public JButton getFreezeButton() {
+        return freezeButton;
+    }
+
+    public BufferedImage getShootSignImage() {
+        return shootSignImage;
     }
 }
